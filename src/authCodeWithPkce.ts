6e1eb9e9ed870ba5fb22848fae5bd67df84639fc
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-04-06 17:27:55 -0300

    teste githubpages

--- src/authCodeWithPkce.ts
+++ src/authCodeWithPkce.ts
@@ -1,5 +1,7 @@
 // Códigos do tutorial do spotify: https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
 
+const redirect_uri = 'https://gregoriofornetti.github.io/spotify-music-guess/dist'  // 'http://localhost:5173/callback'
+
 export async function redirectToAuthCodeFlow(clientId: string) {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
@@ -9,7 +11,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", redirect_uri);
     params.append("scope", "user-read-private user-read-email user-modify-playback-state");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -24,7 +26,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", redirect_uri);
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
