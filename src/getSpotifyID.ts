a2359b4ae4a49055f850a9c592aa1acf259bc105
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-04-06 16:19:22 -0300

    coletando informações de uma playlist

---
+++ src/getSpotifyID.ts
@@ -0,0 +1,40 @@
+
+const id_regex_any_place = "([A-Za-z0-9]{22})"
+const id_regex = new RegExp(`^${id_regex_any_place}$`)
+const uri_regex = new RegExp(`^spotify:playlist:${id_regex_any_place}$`)
+const url_regex = new RegExp(`^https:\/\/open.spotify.com\/playlist\/${id_regex_any_place}(\\?si=.*)?$`)
+
+
+function is_spotify_uri(uri: string): boolean {
+    return uri_regex.test(uri)
+}
+
+function is_spotify_url(url: string): boolean {
+    return url_regex.test(url)
+}
+
+function is_spotify_id(id: string): boolean {
+    return id_regex.test(id)
+}
+
+
+function get_spotify_id_from_uri(uri: string): string {
+    return uri.match(uri_regex)![1]
+}
+
+function get_spotify_id_from_url(url: string): string {
+    return url.match(url_regex)![1]
+}
+
+
+export default function get_spotify_id(user_playlist: string): string {
+    if (is_spotify_uri(user_playlist)) {
+        return get_spotify_id_from_uri(user_playlist)
+    } else if (is_spotify_url(user_playlist)) {
+        return get_spotify_id_from_url(user_playlist)
+    } else if (is_spotify_id(user_playlist)) {
+        return user_playlist
+    } else {
+        throw new Error("URI, URL ou ID inválido.")
+    }
+}
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,37 @@
 // Because this is a literal single page application
 // we detect a callback from Spotify by checking for the hash fragment
 import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
+import get_spotify_id from "./getSpotifyID";
+
+async function start_playing_playlist(playlist_URI: String, accessToken: any) {
+    const test = await fetch("https://api.spotify.com/v1/me/player/play", {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+            "context_uri": playlist_URI,
+            "offset": {
+                "position": 5
+            },
+            "position_ms": 0
+        })
+    })
+    return test
+}
+
+async function get_playlist_info(playlist_id: String, accessToken: any) {
+    const test = fetch(`https://api.spotify.com/v1/playlists/${playlist_id}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        }
+    })
+    .then(response => response.json())
+
+    return test
+}
+
 
 const clientId = "b3c2339a149d46afa94a39347466b623";
 const params = new URLSearchParams(window.location.search);
@@ -10,37 +41,15 @@ if (!code) {
     redirectToAuthCodeFlow(clientId);
 } else {
     const accessToken = await getAccessToken(clientId, code)
-    const test = await fetch("https://api.spotify.com/v1/me/player/play", {
-        method: "PUT",
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-            "context_uri": "spotify:playlist:0BjRyg8AxIdh1DMtVE7t6f",
-            "offset": {
-                "position": 5
-            },
-            "position_ms": 0
-        })
+    document.getElementById("playlist-form")?.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const playlist_input_str = (<HTMLInputElement>document.getElementById("playlist-input")).value;
+        try {
+            const playlist_id = get_spotify_id(playlist_input_str)
+            const result = await get_playlist_info(playlist_id, accessToken);
+            console.log(result)
+        } catch (error) {
+            console.log(error)
+        }
     })
-}
-
-async function fetchProfile(code: string): Promise<UserProfile> {
-    const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${code}` }
-    });
-
-    return await result.json();
-}
-
-function populateUI(profile: UserProfile) {
-    document.getElementById("displayName")!.innerText = profile.display_name;
-    document.getElementById("avatar")!.setAttribute("src", profile.images[0].url)
-    document.getElementById("id")!.innerText = profile.id;
-    document.getElementById("email")!.innerText = profile.email;
-    document.getElementById("uri")!.innerText = profile.uri;
-    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url")!.innerText = profile.href;
-    document.getElementById("url")!.setAttribute("href", profile.href);
-    document.getElementById("imgUrl")!.innerText = profile.images[0].url;
 }
