bc049644cd64584a47587cd687267f0dfc11000d
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-04-24 21:40:00 -0300

    documentação devices

--- src/spotifyApi/requests/getDevices.ts
+++ src/spotifyApi/requests/getDevices.ts
@@ -2,7 +2,13 @@ import Device from "../types/Device"
 import User from "../../global/User"
 
 
-
+/**
+ * 
+ *  Coleta os dispositivos que podem ser utilizados para tocar músicas
+ * 
+ *  @returns uma promessa de um array de objetos do tipo Device, contendo os dispositivos que podem ser utilizados para tocar músicas
+ *  
+ */
 export default async function getDevices(): Promise<Device[]> {
     return fetch("https://api.spotify.com/v1/me/player/devices", {
         headers: User.accessTokenHeader,
--- src/spotifyApi/requests/selectDevice.ts
+++ src/spotifyApi/requests/selectDevice.ts
@@ -1,6 +1,14 @@
 
 import User from "../../global/User"
 
+
+/**
+ *
+ *  Seleciona um dispositivo para tocar músicas
+ * 
+ *  @param {string} deviceId - ID do dispositivo que será selecionado
+ * 
+ */
 export default async function selectDevice(deviceId: string) {
     return fetch("https://api.spotify.com/v1/me/player", {
         headers: User.accessTokenHeader,
