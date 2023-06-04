5fea2f9b020e107c07f07d7cbbc22887719aa4dc
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-06-03 23:02:41 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import toggleToPage from "./utils/pageToggler"
 import User from "./global/User"
 import getUserCountry from "./spotifyApi/requests/getUserCountry"
 import loadHomePage from "./pages/home"
-import modalTeste from "./components/Modals/modalTeste"
 
 const clientId = "b3c2339a149d46afa94a39347466b623";
 const params = new URLSearchParams(window.location.search)
@@ -22,6 +21,4 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         toggleToPage('home-page')
     }
-
-    modalTeste.show()
 })
