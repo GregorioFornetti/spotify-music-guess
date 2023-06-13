95a53a1e1bd7adc011023329dff392923e1acd7f
Guilherme Camargo <guilhermecamargo@estudante.ufscar.br>
2023-06-12 22:04:36 -0300

    Nome e avatar implementados

--- src/gitAPI/authCode.ts
+++ src/gitAPI/authCode.ts
@@ -1,13 +1,12 @@
-export async function getUser() { 
-    const username = 'GregorioFornetti';
-
-    const getUser = () => {
-        fetch(`https://api.github.com/users/${username}/`, {
+export async function getInfo(username: string) { 
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`, {
             headers: {'X-GitHub-Api-Version': '2022-11-28'}
-        })
-        .then(response => response.json()) 
-        .then(data => console.log(data))
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+        return null;
     }
-
-    getUser();
 }
--- src/pages/creditos.ts
+++ src/pages/creditos.ts
@@ -1,13 +1,36 @@
-import { getUser } from "../gitAPI/authCode"
+import { getInfo } from "../gitAPI/authCode"
 import toggleToPage from '../utils/pageToggler'
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("credit")?.addEventListener("click", () => {
         toggleToPage('credits-page')
-        const username = getUser()
-        console.log("username: " + username)
+        //const data = getInfo('guilhermesdc')
+        // pegar o nome do usuário da variavel data
+        // lista de nomes
+        const users = ['guilhermesdc', 'N4NiNi', 'caiopadovan', 'GregorioFornetti', 'A-nita'] 
+        const creditsElement = document.getElementById('credits-page')!;
+        for (let user of users) {
+            const userDataPromise = getInfo(user);
+            userDataPromise.then(userData => {
+                const name = userData.name;
+                const avatar = userData.avatar_url;
+
+                const paragraphElement = document.createElement('p');
+                const avatarElement = document.createElement('img');
+                
+                avatarElement.classList.add('avatar');
+                paragraphElement.classList.add('gitName');
+
+                avatarElement.setAttribute('src', avatar);
+                paragraphElement.textContent = name;
+
+                const divElement = document.createElement('div');
+                divElement.appendChild(paragraphElement);
+                divElement.appendChild(avatarElement);
+
+                divElement.classList.add('div-user');
+                creditsElement.appendChild(divElement);
+            });
         }
-    )   
+    })   
 })
-
-
