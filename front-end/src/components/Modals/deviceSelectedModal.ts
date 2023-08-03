4570cd4cd27273c6c32251d86e151e6270a31a0d
Pandxra <83519905+Pandxra@users.noreply.github.com>
2023-08-02 23:11:58 -0300

    device modal sem palhaçada

--- front-end/src/components/Modals/deviceSelectedModal.ts
+++ front-end/src/components/Modals/deviceSelectedModal.ts
@@ -1,12 +1,3 @@
-/* checar se o botão começar jogo foi selecionado
-adicionar um event-listenner
-caso nenhum device tenha sido selecionado adicionar um pop-up com mensagem e com os modais
- disponíveis para seleção , impedir que o usuário migre para a página seguinte
- antes de selecionar o dispositivo
-*/
-
-
-
 import Modal from "./base/visibleModal"
 import getDevices from "../../spotifyApi/requests/getDevices"
 import createDeviceElement from "../device"
@@ -27,12 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         /* se a div estiver vazia nenhum device foi selecionado o usuário deve ser orientado a abrir o spotify */
         if (devicesList?.innerHTML.trim() === '') {
-            console.log("nenhum dispositivo encontrado, favor abrir o spotfy, idiota.")
-
             modalContent = document.createElement('div');
             var paragrafo = document.createElement('p');
             modalContent.className = 'mensagem-devices'
-            paragrafo.textContent = 'nenhum dispositivo encontrado, favor abrir o spotfy, idiota.'
+            paragrafo.textContent = 'nenhum dispositivo encontrado, favor abrir o spotfy.'
             modalContent.appendChild(paragrafo);
 
             deviceSelectedModal.addContent(modalContent)
@@ -42,7 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
             /* se houverem dispositivos na div, podem estar selecionados ou não
             caso não tenha nenhum dispositivo selecionado pedir ao usuário para selecionar um dispositivo, se não deixa passar */
             console.log('tem device, agora seleciona')
-            //let selected = devicesList?.getElementsByClassName('selected');
             /* se não tem elementos selecionados */
             const premium = document.getElementById('input-premium') as HTMLInputElement
         
