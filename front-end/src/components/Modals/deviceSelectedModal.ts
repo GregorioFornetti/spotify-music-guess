9cb381c749936d4152fff6b3d3560a2086d1d441
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-08-19 00:55:08 -0300

    deviceSelectModal

--- front-end/src/components/Modals/deviceSelectedModal.ts
+++ front-end/src/components/Modals/deviceSelectedModal.ts
@@ -1,65 +1,70 @@
-
 import Modal from "./base/visibleModal"
 import getDevices from "../../spotifyApi/requests/getDevices"
 import createDeviceElement from "../device"
 import selectDevice from "../../spotifyApi/requests/selectDevice"
 
-const deviceSelectedModal = new Modal('deviceSelectedModal', 'modal-devices', false, true)
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('inicializando o modal')
-
-    const initGame = document.getElementById('configs-rounds-form')
-
-    console.log(initGame)
-    var modalContent
-
-    initGame?.addEventListener('submit', function () {
+const deviceSelected = new Modal('deviceSelectedModal', 'modal-devices', false, true)
+var modalContent
+class deviceSelectedModal {
     
-        let devicesList = document.getElementById('devices-list');
+    
 
-        // se a div estiver vazia nenhum device foi selecionado o usuário deve ser orientado a abrir o spotify
-        if (devicesList?.innerHTML.trim() === '') {
-            modalContent = document.createElement('div');
-            var paragrafo = document.createElement('p');
-            modalContent.className = 'mensagem-devices'
-            paragrafo.textContent = 'nenhum dispositivo encontrado, favor abrir o spotfy.'
-            modalContent.appendChild(paragrafo);
-
-            deviceSelectedModal.addContent(modalContent)
-            deviceSelectedModal.show()
-
-        } else {
-            // se houverem dispositivos na div, podem estar selecionados ou não
-            // caso não tenha nenhum dispositivo selecionado pedir ao usuário para selecionar um dispositivo, se não deixa passar 
-            console.log('tem device, agora seleciona')
-            // se não tem elementos selecionados
-            const premium = document.getElementById('input-premium') as HTMLInputElement
+    static show(onDeviceSelect: () => void): void {
         
-            if (premium) {
-                getDevices().then(devices => {
-                 
-                    console.log(devices.length)
 
-                    const devicesListElement = document.createElement('div')
+       
+        getDevices().then(devices => {
+            
+            console.log(devices.length)
+            /* não tem dispositivo nenhum, abrir algum dispositivo */
+            if (devices.length < 1) {
 
-                    for (const device of devices) {
-                        const deviceElement = createDeviceElement(device)
-                        deviceElement.addEventListener('click', async () => {
-                            if (device.id) {
-                                await selectDevice(device.id)
-                                deviceSelectedModal.hide()
-                            }
-                        })
-                        devicesListElement.appendChild(deviceElement)
-                    }
-                    if (!devices.some((device) => device.is_active)) {
-                        deviceSelectedModal.addContent(devicesListElement)
-                        deviceSelectedModal.show()
-                    }
+                modalContent = document.createElement('div');
+                var paragrafo = document.createElement('p');
+                modalContent.className = 'mensagem-devices'
+                paragrafo.textContent = 'nenhum dispositivo encontrado, favor abrir o spotfy.'
+                modalContent.appendChild(paragrafo);
+
+                const botao = document.createElement('button')
+                botao.innerHTML = 'recarregar devices'
+                botao.addEventListener('click', () => {
+                    deviceSelected.hide()
+                    this.show(onDeviceSelect)   
                 })
+                modalContent.appendChild(botao)
+
+                deviceSelected.addContent(modalContent)
+                deviceSelected.show()
+
             }
-        }   
-        }) 
-})
+            else {
+
+                const devicesListElement = document.createElement('div')
+
+                for (const device of devices) {
+                    const deviceElement = createDeviceElement(device)
+                    deviceElement.addEventListener('click', async () => {
+                        if (device.id) {
+                            selectDevice(device.id).then(() => {
+                                deviceSelected.hide()
+                                onDeviceSelect()
+                            })
+                            
+                        }
+                    })
+                    devicesListElement.appendChild(deviceElement)
+                }
+                if (!devices.some((device) => device.is_active)) {
+                    deviceSelected.addContent(devicesListElement)
+                    deviceSelected.show()
+                }
+                else {
+                    onDeviceSelect()
+                }
+            }
+        })
+    
+    }
+}
 
 export default deviceSelectedModal
